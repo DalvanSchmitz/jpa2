2520468a0d28efe094a3a809a76a22928a294e77
Dalvan Schmitz <dalvan.schmitz@publichess.com.br>
2019-02-05 08:34:40 -0200

    alteracoes

--- src/file/ImportCSV.java
+++ src/file/ImportCSV.java
@@ -1,11 +1,6 @@
 package file;
 
-import static file.FileUtils.dateValue;
-import static file.FileUtils.decimalValue;
-import static file.FileUtils.getColuna;
-import static file.FileUtils.intValue;
-import static file.FileUtils.stringValue;
-import static file.FileUtils.validarExtensao;
+import static file.FileUtils.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -19,83 +14,104 @@ import dao.CartaoDAO;
 import domain.Bandeira;
 import domain.Cartao;
 import domain.TipoTransacao;
-import old.CartaoDAOOld;
-import old.CartaoDAOJPA;
 
 public class ImportCSV {
 
-	@SuppressWarnings("resource")
-	public static void execute(String arquivoOrigem) throws Exception {
-		try {
+    @SuppressWarnings("resource")
+    public static void execute(String arquivoOrigem) throws Exception {
+        try {
 
-			validarExtensao(arquivoOrigem);
+            validarExtensao(arquivoOrigem);
 
-			File arquivoCSV = new File(arquivoOrigem);
+            File arquivoCSV = new File(arquivoOrigem);
 
-			Scanner leitor = new Scanner(arquivoCSV);
-			String linhasDoArquivo = new String();
+            Scanner leitor = new Scanner(arquivoCSV);
+            String linhasDoArquivo = new String();
 
-			String colunasIniciais = leitor.nextLine();
-			// metodo para conferir as colunas da primeira linha
-			// fileUtils.conferirColunasIniciais(colunasIniciais) ;
+            String colunasIniciais = leitor.nextLine();
+            // metodo para conferir as colunas da primeira linha
+            // fileUtils.conferirColunasIniciais(colunasIniciais) ;
 
-			// metodo realizado para validar a primeira linha de acordo com os
-			// campos descritos no arquivo cvs
-			// fileUtils.conferirPrimeiraLinha(colunasIniciais);
-			List<Cartao> cartoes = new ArrayList<>();
-			while (leitor.hasNext()) {
-				linhasDoArquivo = leitor.nextLine();
-				cartoes.add(getCartao(linhasDoArquivo));
-			}
+            // metodo realizado para validar a primeira linha de acordo com os
+            // campos descritos no arquivo cvs
+            // fileUtils.conferirPrimeiraLinha(colunasIniciais);
+            List<Cartao> cartoes = new ArrayList<>();
+            while (leitor.hasNext()) {
+                linhasDoArquivo = leitor.nextLine();
+                cartoes.add(getCartao(linhasDoArquivo));
+            }
 
-			save(cartoes);
+            save(getCartoes(cartoes));
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private List<Cartao> getListBD(){
-		try {
-			return new CartaoDAO().findAll();
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Problemas ao localizar Cartões " + e.getLocalizedMessage());
-		}
-		return null;
-	}
-	
-	
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private static void save(List<Cartao> cartoes) {
-		CartaoDAO dao = new CartaoDAO();
-		dao.salvar(cartoes);
-		
-		dao = new CartaoDAO();
-		
-		try {
-			List<Cartao> list = dao.findAll();
-			System.out.println(list);
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Problemas ao localizar contaton" + e.getLocalizedMessage());
-		}
+    private static List<Cartao> getCartoes(List<Cartao> cartoes) {
+        boolean existe = false;
+        List<Cartao> listBD = getListBD();
+        List<Cartao> cartoesAux = new ArrayList<>();
 
-		JOptionPane.showMessageDialog(null, "Dados importados com sucesso!");
-	}
+        if (listBD.isEmpty()) {
+            return cartoes;
+        }
 
-	private static Cartao getCartao(String linhasDoArquivo) {
-		String[] colunas = linhasDoArquivo.split(";");
-		Cartao c = new Cartao();
-		c.setDtTransacao(dateValue(colunas[0]));
-		c.setTipoTransacaoId(TipoTransacao.toEnum(getColuna(colunas[1])).getCod());
-		c.setBandeiraId(Bandeira.toEnum(getColuna(colunas[2])).getCod());
-		c.setMeioCaptura(stringValue(colunas[3]));
-		c.setStoneId(stringValue(colunas[4]));
-		c.setVlrBruto(decimalValue(colunas[5]));
-		c.setVlrLiquido(decimalValue(colunas[6]));
-		c.setNrCartao(stringValue(colunas[7]));
-		c.setSerialNember(intValue(colunas[8]));
-		c.setUltStatus(stringValue(colunas[9]));
-		c.setDtUltStatus(dateValue(colunas[10]));
-		return c;
-	}
+        for (Cartao c : cartoes) {
+            existe = false;
+            for (Cartao bd : listBD) {
+                if (bd.getDtTransacao().equals(c.getDtTransacao()) && bd.getNrCartao().equals(c.getNrCartao())) {
+                    existe = true;
+                    break;
+                }
+            }
+            if (!existe) {
+                cartoesAux.add(c);
+            }
+        }
+
+        return cartoesAux;
+    }
+
+    private static List<Cartao> getListBD() {
+        try {
+            return new CartaoDAO().findAll();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Problemas ao localizar Cartï¿½es " + e.getLocalizedMessage());
+        }
+        return null;
+    }
+
+    private static void save(List<Cartao> cartoes) {
+        CartaoDAO dao = new CartaoDAO();
+        dao.salvar(cartoes);
+
+        dao = new CartaoDAO();
+
+        try {
+            List<Cartao> list = dao.findAll();
+            System.out.println(list);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Problemas ao localizar contaton" + e.getLocalizedMessage());
+        }
+
+        JOptionPane.showMessageDialog(null, "Dados importados com sucesso!");
+    }
+
+    private static Cartao getCartao(String linhasDoArquivo) {
+        String[] colunas = linhasDoArquivo.split(";");
+        Cartao c = new Cartao();
+        c.setDtTransacao(dateValue(colunas[0]));
+        c.setTipoTransacaoId(TipoTransacao.toEnum(getColuna(colunas[1])).getCod());
+        c.setBandeiraId(Bandeira.toEnum(getColuna(colunas[2])).getCod());
+        c.setMeioCaptura(stringValue(colunas[3]));
+        c.setStoneId(stringValue(colunas[4]));
+        c.setVlrBruto(decimalValue(colunas[5]));
+        c.setVlrLiquido(decimalValue(colunas[6]));
+        c.setNrCartao(stringValue(colunas[7]));
+        c.setSerialNember(intValue(colunas[8]));
+        c.setUltStatus(stringValue(colunas[9]));
+        c.setDtUltStatus(dateValue(colunas[10]));
+        return c;
+    }
 }
